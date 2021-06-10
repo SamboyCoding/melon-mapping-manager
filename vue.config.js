51959aa31320a68c85040b3c273a5eef5008fc4d
Sam Byass <sam.byass@warwick.ac.uk>
2021-06-10 12:23:32 +0100

    Add vue config file

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    productionSourceMap: false,
+    configureWebpack: {
+        plugins: [
+
+        ],
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+                minSize: 20000,
+                cacheGroups: {
+                    default: false,
+                    vendors: false,
+                    vue: {
+                        chunks: 'all',
+                        test: /node_modules\\vue/
+                    },
+                    vendor: {
+                        chunks: 'all',
+                        test: /node_modules/
+                    }
+                }
+            }
+        }
+    }
+};
